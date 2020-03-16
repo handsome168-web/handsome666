599496545772f50d21101398c9c523c502a492fc
git config --globla user.name handsome168-web <19451407@life.hkbu.edu.hk>
2020-03-17 01:25:24 +0800

    1

--- assignment3.py
+++ assignment3.py
@@ -1,63 +1,124 @@
-import os, sys
+from __future__ import unicode_literals
 
-from flask import Flask, request, abort, jsonify
-import requests
+import os
+import sys
+import redis
 
+from argparse import ArgumentParser
 
+from flask import Flask, request, abort
 from linebot import (
-    LineBotApi, WebhookHandler
+    LineBotApi, WebhookParser
 )
 from linebot.exceptions import (
     InvalidSignatureError
 )
+
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage, ImageMessage, VideoMessage, FileMessage, StickerMessage, StickerSendMessage
 )
+from linebot.utils import PY3
 
 app = Flask(__name__)
 
 # get channel_secret and channel_access_token from your environment variable
 channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
 channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
-if channel_secret is None or channel_access_token is None:
-    print('Specify LINE_CHANNEL_SECRET and LINE_CHANNEL_ACCESS_TOKEN as environment variables.')
+
+# obtain the port that heroku assigned to this app.
+heroku_port = os.getenv('PORT', None)
+
+if channel_secret is None:
+    print('Specify LINE_CHANNEL_SECRET as environment variable.')
+    sys.exit(1)
+if channel_access_token is None:
+    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
     sys.exit(1)
 
 line_bot_api = LineBotApi(channel_access_token)
-handler = WebhookHandler(channel_secret)
+parser = WebhookParser(channel_secret)
 
 
-@app.route('/callback', methods=['POST'])
+@app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
     # get request body as text
     body = request.get_data(as_text=True)
-    app.logger.info('Request body: ' + body)
+    app.logger.info("Request body: " + body)
 
-    # handle webhook body
+    # parse webhook body
     try:
-        handler.handle(body, signature)
+        events = parser.parse(body, signature)
     except InvalidSignatureError:
-        print('Invalid signature. Please check your channel access token/channel secret.')
         abort(400)
+
+    # if event is MessageEvent and message is TextMessage, then echo text
+    for event in events:
+        if not isinstance(event, MessageEvent):
+            continue
+        if isinstance(event.message, TextMessage):
+            handle_TextMessage(event)
+        if isinstance(event.message, ImageMessage):
+            handle_ImageMessage(event)
+        if isinstance(event.message, VideoMessage):
+            handle_VideoMessage(event)
+        if isinstance(event.message, FileMessage):
+            handle_FileMessage(event)
+        if isinstance(event.message, StickerMessage):
+            handle_StickerMessage(event)
+
+        if not isinstance(event, MessageEvent):
+            continue
+        if not isinstance(event.message, TextMessage):
+            continue
 
     return 'OK'
 
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    input_text = event.message.text
+# Handler function for Text Message
+def handle_TextMessage(event):
+    print(event.message.text)
+    msg = 'You said: "' + event.message.text + '" '
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(msg)
+    )
 
-    if input_text == '@查詢匯率':
-        resp = requests.get('https://tw.rter.info/capi.php')
-        currency_data = resp.json()
-        usd_to_twd = currency_data['USDTWD']['Exrate']
+# Handler function for Sticker Message
+def handle_StickerMessage(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        StickerSendMessage(
+            package_id=event.message.package_id,
+            sticker_id=event.message.sticker_id)
+    )
 
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=f'美元 USD 對台幣 TWD：1:{usd_to_twd}'))
+# Handler function for Image Message
+def handle_ImageMessage(event):
+    line_bot_api.reply_message(
+	event.reply_token,
+	TextSendMessage(text="Nice image!")
+    )
 
+# Handler function for Video Message
+def handle_VideoMessage(event):
+    line_bot_api.reply_message(
+	event.reply_token,
+	TextSendMessage(text="Nice video!")
+    )
 
-if __name__ == '__main__':
-    app.run()
+# Handler function for File Message
+def handle_FileMessage(event):
+    line_bot_api.reply_message(
+	event.reply_token,
+	TextSendMessage(text="Nice file!")
+    )
+
+if __name__ == "__main__":
+    arg_parser = ArgumentParser(
+        usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
+    )
+    arg_parser.add_argument('-d', '--debug', default=False, help='debug')
+    options = arg_parser.parse_args()
+
+    app.run(host='0.0.0.0', debug=options.debug, port=heroku_port)
