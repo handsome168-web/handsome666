2ee0f36ab6431d6c265d165f379fdff74c952cb0
git config --globla user.name handsome168-web <19451407@life.hkbu.edu.hk>
2020-03-16 23:38:43 +0800

    2

--- assignment3.py
+++ assignment3.py
@@ -1,61 +1,49 @@
-from __future__ import unicode_literals
-
-import os
-import sys
-import redis
-
-from argparse import ArgumentParser
+import os, sys
 
 from flask import Flask, request, abort, jsonify
+import requests
+
+
 from linebot import (
-    LineBotApi, WebhookParser
+    LineBotApi, WebhookHandler
 )
 from linebot.exceptions import (
     InvalidSignatureError
 )
-
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, ImageMessage, VideoMessage, FileMessage, StickerMessage, StickerSendMessage
+    MessageEvent, TextMessage, TextSendMessage,
 )
-from linebot.utils import PY3
 
 app = Flask(__name__)
 
 # get channel_secret and channel_access_token from your environment variable
 channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
 channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
-
-# obtain the port that heroku assigned to this app.
-heroku_port = os.getenv('PORT', None)
-
-if channel_secret is None:
-    print('Specify LINE_CHANNEL_SECRET as environment variable.')
-    sys.exit(1)
-if channel_access_token is None:
-    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
+if channel_secret is None or channel_access_token is None:
+    print('Specify LINE_CHANNEL_SECRET and LINE_CHANNEL_ACCESS_TOKEN as environment variables.')
     sys.exit(1)
 
 line_bot_api = LineBotApi(channel_access_token)
-parser = WebhookParser(channel_secret)
+handler = WebhookHandler(channel_secret)
 
 
-@app.route("/callback", methods=['POST'])
+@app.route('/callback', methods=['POST'])
 def callback():
+    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
     # get request body as text
     body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+    app.logger.info('Request body: ' + body)
 
-    # parse webhook body
+    # handle webhook body
     try:
-        events = parser.parse(body, signature)
+        handler.handle(body, signature)
     except InvalidSignatureError:
+        print('Invalid signature. Please check your channel access token/channel secret.')
         abort(400)
+
     return 'OK'
-
-
-
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
@@ -71,17 +59,5 @@ def handle_message(event):
             TextSendMessage(text=f'美元 USD 對台幣 TWD：1:{usd_to_twd}'))
 
 
-
-
-
-
-
-
-if __name__ == "__main__":
-    arg_parser = ArgumentParser(
-        usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
-    )
-    arg_parser.add_argument('-d', '--debug', default=False, help='debug')
-    options = arg_parser.parse_args()
-
-    app.run(host='0.0.0.0', debug=options.debug, port=heroku_port)
+if __name__ == '__main__':
+    app.run()
