056759be613ba5e8085b90b3e68fd60ae9410b51
handsome168-web <56117925+handsome168-web@users.noreply.github.com>
2020-03-22 18:27:20 +0800

    Update app_run.py

--- app_run.py
+++ app_run.py
@@ -56,5 +56,4 @@ def reply_text_message(event):
 #     app.run()
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=heroku_port)
