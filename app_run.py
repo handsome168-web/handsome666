2dcc43eaf785d175c38121e8ce47a469e1eb127b
handsome168-web <56117925+handsome168-web@users.noreply.github.com>
2020-03-22 17:39:12 +0800

    Update app_run.py

--- app_run.py
+++ app_run.py
@@ -7,6 +7,8 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 import configparser
 
+from custom_models import ChannelTalks #, ChannelFlex, utils
+
 app = Flask(__name__)
 
 # get LINE tokens from config.ini
@@ -34,11 +36,30 @@ def callback():
 
 # repeat text message
 @handler.add(MessageEvent, message=TextMessage)
-def echo(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text)
-    )
+# list out all reply options:
+def reply_text_message(event):
+    print(event)
+    text = event.message.text
+
+    if (text == "symptoms of COVID-19" or text == "symptoms"):
+        reply_text = "Fever,Cough,Shortness of breath or difficulty breathing,Tiredness,Aches,Runny nose and Sore throat"
+    elif (text == "Protection" or text == "Precaution"):
+        reply_text = "1,clean your hands for at least 20 seconds with soap and water, or use an alcohol-based sanitiser with at least 70% alcohol.2,cover your sneeze or cough with your elbow or with tissue.3,avoid close contact with people who are ill.4,avoid touching your eyes, nose and mouth."
+    elif (text == "Risk factors"):
+        reply_text = "1,Recent travel from or residence in an area with ongoing community spread of COVID-19 as determined by CDC or WHO.2,Close contact with someone who has COVID-19 — such as when a family member or health care worker takes care of an infected person"
+    elif event.source.user_id != "Udeadbeefdfeadfsdlkfdasofjewa":
+        reply = False #not yet replied
+
+        #trying reply by condition:
+        if not reply:
+            reply = ChannelTalks.location_search(event)
+        #***
+        #To add other reply options
+        #***
+        #finally, if not get replied yet:
+        if not reply:
+            reply = ChannelTalks.echo(event)
+
 
 if __name__ == "__main__":
     app.run()
