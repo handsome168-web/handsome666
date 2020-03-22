8787df934d3e293009092c0bd1fb9272f39802b9
handsome168-web <56117925+handsome168-web@users.noreply.github.com>
2020-03-22 18:07:16 +0800

    Update app_run.py

--- app_run.py
+++ app_run.py
@@ -47,7 +47,7 @@ def reply_text_message(event):
         reply_text = "1,clean your hands for at least 20 seconds with soap and water, or use an alcohol-based sanitiser with at least 70% alcohol.2,cover your sneeze or cough with your elbow or with tissue.3,avoid close contact with people who are ill.4,avoid touching your eyes, nose and mouth."
     elif (text == "Risk factors"):
         reply_text = "1,Recent travel from or residence in an area with ongoing community spread of COVID-19 as determined by CDC or WHO.2,Close contact with someone who has COVID-19 — such as when a family member or health care worker takes care of an infected person"
-    elif event.source.user_id != "Udeadbeefdfeadfsdlkfdasofjewa":
+    else event.source.user_id != "Udeadbeefdfeadfsdlkfdasofjewa":
         reply = False #not yet replied
 
         #trying reply by condition:
@@ -64,7 +64,7 @@ def reply_text_message(event):
 
 # if __name__ == "__main__":
 #     app.run()
-import os
+
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
