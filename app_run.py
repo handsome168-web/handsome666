04bd624ed534a169d72ed465ee6edbb14c6e9d40
handsome168-web <56117925+handsome168-web@users.noreply.github.com>
2020-03-29 18:09:04 +0800

    Update app_run.py

--- app_run.py
+++ app_run.py
@@ -9,9 +9,9 @@ from urllib.request import urlopen
 import configparser
 from linebot.models import *
 
-HOST = "redis-13333.c56.east-us.azure.cloud.redislabs.com"
-PWD = "ubZLeDUxIKCYKBHK15dtY3TjfnmPw824"
-PORT = "13333"
+# HOST = "redis-13333.c56.east-us.azure.cloud.redislabs.com"
+# PWD = "ubZLeDUxIKCYKBHK15dtY3TjfnmPw824"
+# PORT = "13333"
 
 redis1 = redis.Redis(host = HOST, password = PWD, port = PORT)
 # from custom_models import ChannelTalks #, ChannelFlex, utils
