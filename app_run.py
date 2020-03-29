49faf4fec961673bc75195b5445e283be30332ce
handsome168-web <56117925+handsome168-web@users.noreply.github.com>
2020-03-29 17:08:11 +0800

    Update app_run.py

--- app_run.py
+++ app_run.py
@@ -36,13 +36,13 @@ def callback():
         abort(400)
 
     return 'OK'
-print("Wilson is very very handsome!")
+
 HOST = "redis-13333.c56.east-us.azure.cloud.redislabs.com"
 PWD = "ubZLeDUxIKCYKBHK15dtY3TjfnmPw824"
 PORT = "13333"
-print("Wilson is very handsome!")
+
 redis1 = redis.Redis(host = HOST, password = PWD, port = PORT)
-print("Wilson is very cool!")
+
 redis1.set("symptoms", "Fever,Cough,Shortness of breath or difficulty breathing,Tiredness,Aches,Runny nose and Sore throat")
 # redis1.set("protection", "1,clean your hands for at least 20 seconds with soap and water, or use an alcohol-based sanitiser with at least 70% alcohol.2,cover your sneeze or cough with your elbow or with tissue.3,avoid close contact with people who are ill.4,avoid touching your eyes, nose and mouth.")
 # redis1.set("risk factors", "1,Recent travel from or residence in an area with ongoing community spread of COVID-19 as determined by CDC or WHO.2,Close contact with someone who has COVID-19 — such as when a family member or health care worker takes care of an infected person")
@@ -56,7 +56,9 @@ def reply_text_message(event):
     text = event.message.text
 
     if (text == "symptoms of COVID-19" or text == "symptoms"):
-        print("Wilson is handsome!")
+        reply_text = "handsome"
+        message = TextSendMessage(reply_text)
+        line_bot_api.reply_message(event.reply_token, message)
         reply_text = redis1.get("symptoms").decode('UTF-8')
     elif (text == "protection" or text == "precaution"):
         reply_text = redis1.get("protection").decode('UTF-8')
