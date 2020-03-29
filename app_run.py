eb802df6ef1f45726dd9a474b5a63afac0570b07
handsome168-web <56117925+handsome168-web@users.noreply.github.com>
2020-03-29 22:00:52 +0800

    Update app_run.py

--- app_run.py
+++ app_run.py
@@ -50,7 +50,7 @@ def reply_text_message(event):
     print(event)
     text = event.message.text
     text = text.strip()
-    if ((re.findall("(symptom)",text,re.I)[0] != None):
+    if (re.findall("(symptom)",text,re.I)[0] != None or re.findall("(feature)",text,re.I)[0] != None):
         reply_text = redis1.get("symptoms").decode('UTF-8')
     elif (text == "protection" or text == "precaution"):
         reply_text = redis1.get("protection").decode('UTF-8')   
