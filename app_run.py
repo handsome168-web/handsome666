25dacae778ce81cb38371f5823b5af7522fad565
handsome168-web <56117925+handsome168-web@users.noreply.github.com>
2020-03-30 21:12:52 +0800

    Update app_run.py

--- app_run.py
+++ app_run.py
@@ -51,20 +51,11 @@ def reply_text_message(event):
     print(event)
     text = event.message.text
 
-    try:
-        if (re.findall("(symptom)", text, re.I)[0] != None):
-            reply_text = redis1.get("symptoms").decode('UTF-8')
-    except:
-        try:
-            if (re.findall("(protection)", text, re.I)[0] != None or re.findall("(precaution)", text, re.I)[0] != None):
-                reply_text = redis1.get("protection").decode('UTF-8')
-        except:
-            try:
-                if (re.findall("(risk factors)", text, re.I)[0] != None):
-                    reply_text = redis1.get("risk factors").decode('UTF-8')
-            except:
-                reply_text = text
-    if (event.source.user_id != "Udeadbeefdfeadfsdlkfdasofjewa"):
+    
+    if (re.findall("(symptom)", text, re.I)[0] != None):
+        reply_text = redis1.get("symptoms").decode('UTF-8')
+    
+    elif (event.source.user_id != "Udeadbeefdfeadfsdlkfdasofjewa"):
         reply = False #not yet replied
 
         #trying reply by condition:
