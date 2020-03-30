84441995a80f647161edfb3ebcff02e2f7242813
handsome168-web <56117925+handsome168-web@users.noreply.github.com>
2020-03-30 22:14:39 +0800

    Update app_run.py

--- app_run.py
+++ app_run.py
@@ -54,7 +54,7 @@ def reply_text_message(event):
         reply_text = redis1.get("symptoms").decode('UTF-8')
     elif (re.findall("(protection)", text, re.I) or re.findall("(precaution)", text, re.I)):
         reply_text = redis1.get("protection").decode('UTF-8')
-    elif (re.findall("(risk)", text, re.I) or re.findall("(factor)", text, re.I):
+    elif (re.findall("(risk)", text, re.I) or re.findall("(factor)", text, re.I)):
         reply_text = redis1.get("risk factors").decode('UTF-8')
     
     elif (event.source.user_id != "Udeadbeefdfeadfsdlkfdasofjewa"):
